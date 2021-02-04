bce577f45d29aeee15826ff07ad61d40036d625f
PatrickBoynton <jpboynton3@gmail.com>
2021-02-04 15:23:49 -0500

    Changed a and b to be more meaningful.

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,16 @@ console.log("Hello world!");
 
 let battleSim = () => {
   // For testing purposes.
-  let a = 100;
-  let b = 50;
+  let player1 = 100;
+  let player2 = 50;
 
-  while (a > 0 && b > 0) {
-    a--;
-    b--;
-    console.log(a--);
-    console.log(b--);
+  while (player1 > 0 && player2 > 0) {
+    player1--;
+    player2--;
+    console.log(`Player 1 hit points: ${player1}`);
+    console.log(`Player 2 hit points: ${player2}`);
   }
-  if (a > 0) {
+  if (player1 > 0) {
     alert("Player 1 wins!");
   } else {
     alert("Player 2 wins!");
