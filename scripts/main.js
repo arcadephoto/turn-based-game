abd5955636ac8e1f44592c10b9422a595f45d7b0
PatrickBoynton <jpboynton3@gmail.com>
2021-02-04 15:19:35 -0500

    Added a battle sim function.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
 console.log("Hello world!");
+
+let battleSim = () => {
+  // For testing purposes.
+  let a = 100;
+  let b = 50;
+
+  while (a > 0 && b > 0) {
+    a--;
+    b--;
+    console.log(a--);
+    console.log(b--);
+  }
+  if (a > 0) {
+    alert("Player 1 wins!");
+  } else {
+    alert("Player 2 wins!");
+  }
+}
+
+battleSim();
