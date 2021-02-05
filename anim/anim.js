9ab004f51cb59702a0e863226f0a0f5334794884
arcadephoto <75449898+arcadephoto@users.noreply.github.com>
2021-02-05 07:47:06 -0500

    added fight bell sound

--- anim/anim.js
+++ anim/anim.js
@@ -1,10 +1,11 @@
-
+var snd = new Audio("bell.mp3");
 let leftAttack = document.getElementById("ninjaStar");
 function doFight(){
   leftAttack.setAttribute('class', 'leftWeapon');
   document.getElementById('ninjaStar').hidden = false;
   document.body.style.background = "url('landfight.jpg') center";
   document.getElementById('ninjaStarRight').hidden = false;
+  snd.play();
 }
 
 function noFight(){
