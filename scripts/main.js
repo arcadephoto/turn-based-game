9112cfbdca60cb9e47e8a0ba789d17a83f1d1148
PatrickBoynton <jpboynton3@gmail.com>
2021-02-05 13:03:16 -0500

    Added damage functionality.

--- scripts/main.js
+++ scripts/main.js
@@ -6,23 +6,33 @@ let battleSim = () => {
   let player1Weapon = "Laser";
   let player2Name = "Ogre Bob";
   let player2Weapon = "Machine Gun";
+  let player1WeaponDamage = 12;
+  let player2WeaponDamage = 10;
   let player1Hitpoints = Math.floor(Math.random() * 100) + 1;
   let player2Hitpoints = Math.floor(Math.random() * 100) + 1;
 
   while (player1Hitpoints > 0 && player2Hitpoints > 0) {
-    let randomMissfire = Math.random();
-    console.log(randomMissfire.toFixed(2));
-    if (randomMissfire > .5) {
-        console.log("Hit");
-        player1Hitpoints--;
-        player2Hitpoints--;
-        console.log(`${player1Name} hit points: ${player1Hitpoints}`);
-        console.log(`${player2Name} hit points: ${player2Hitpoints}`);
+    let player1randomMissfire = Math.random();
+    let player2RandomMissfire = Math.random();
+
+    console.log(player1randomMissfire.toFixed(2));
+    if (player1randomMissfire > .5) {
+      console.log("Hit");
+      player1Hitpoints-=player2WeaponDamage;
+      console.log(`${player1Name} hit points: ${player1Hitpoints}`);
     } else {
-      console.log("Miss");
+      console.log(`${player1Name} missed!`);
+    }
+
+    if (player2RandomMissfire > .5) {
+      player2Hitpoints-=player1WeaponDamage;
+      console.log(`${player2Name} hit points: ${player2Hitpoints}`);
+    } else {
+      console.log(`${player2Name} missed!`);
     }
 
   }
+  
   if (player1Hitpoints > 0) {
     alert(`${player1Name} won!`);
   } else {
@@ -31,26 +41,22 @@ let battleSim = () => {
 };
 ///////////////////////////////////////////////CHARACTERS
 
-function Character({name, power, weapon, hitPoints} = {}) {
+function Character({ name, power, weapon, hitPoints } = {}) {
   this.name = name;
   this.power = power;
   this.weapon = weapon;
   this.hitPoints = hitPoints;
-};
+}
 
-  function Enemy ({name, power, weapon, hitPoints} = {}) {
-    this.name = name;
-    this.power = power;
-    this.weapon = weapon;
-    this.hitPoints = hitPoints;
-  };
+function Enemy({ name, power, weapon, hitPoints } = {}) {
+  this.name = name;
+  this.power = power;
+  this.weapon = weapon;
+  this.hitPoints = hitPoints;
+}
 
-const characterOne = new Character("Wizard","Energy Blast","Staff","1000");
-const characterTwo = new Character("Knight","Super Swing","Sword","1100");
-const characterThree = new Character("")
-
-
-
-
+const characterOne = new Character("Wizard", "Energy Blast", "Staff", "1000");
+const characterTwo = new Character("Knight", "Super Swing", "Sword", "1100");
+const characterThree = new Character("");
 
 battleSim();
